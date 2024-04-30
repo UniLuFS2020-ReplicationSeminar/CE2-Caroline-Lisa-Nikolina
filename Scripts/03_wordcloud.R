e93e09c05dcb25b38c764b138828809b020977fc
nikmalb <malbasic.nikolina@gmail.com>
2024-04-30 21:53:24 +0200

    adjust the wordcloud with stemming and lemmitization

--- Scripts/03_wordcloud.R
+++ Scripts/03_wordcloud.R
@@ -1,18 +1,35 @@
 # Install and load required packages
 install.packages("tm")
 install.packages("wordcloud")
+install.packages("udpipe")
 library(tm)
 library(wordcloud)
 library(readr)
+library(udpipe)
 
 # Convert titles to a corpus
 all_articles <- read_csv("all_articles.csv")
 titles_corpus <- Corpus(VectorSource(all_articles$title))
 
+# Function for stemming
+stemmer <- function(word) wordStem(word, language = "english")
+
+#Function for lemmitization
+lemmatizer <- function(sentence) {
+  udpipe_model <- udpipe_download_model(language = "english")
+  udpipe_annotate(udpipe_model, x = sentence, doc_id = 1)$lemma
+}
+
 # Preprocess the text: remove punctuation, convert to lowercase, remove stopwords
 titles_corpus <- tm_map(titles_corpus, content_transformer(tolower))
 titles_corpus <- tm_map(titles_corpus, removePunctuation)
 titles_corpus <- tm_map(titles_corpus, removeWords, stopwords("en"))
+
+# Stem the words
+titles_corpus <- tm_map(titles_corpus, content_transformer(stemmer))
+
+# Lemmatize the words
+titles_corpus <- tm_map(titles_corpus, content_transformer(lemmatizer))
 
 # Create a document term matrix
 dtm <- DocumentTermMatrix(titles_corpus)
@@ -23,5 +40,11 @@ m <- as.matrix(dtm)
 # Get word frequencies
 word_freq <- colSums(m)
 
-# Create a word cloud
+# Set the size of the plot
+png("wordcloud.png", width = 1200, height = 1200)
+
+# Create a word cloud with the 100 most frequent words
 wordcloud(names(word_freq), word_freq, max.words = 100, random.order = FALSE, colors = brewer.pal(8, "Dark2"))
+
+# Save the plot
+dev.off()
